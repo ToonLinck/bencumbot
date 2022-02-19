fede4b182f14ad82b40c78192d026c1e31265e95
ToonLinck <Johanlinck96@gmail.com>
2022-02-19 16:03:38 +0100

    increase time

--- bot.js
+++ bot.js
@@ -2,23 +2,27 @@
 
 //names.json is initiated as an object
 const data = require('./names.json')
+
+/* 
+create a config.json file in the same directory as bot.js with the following syntax
+
+{
+
+    "token": "*YOUR BOTS TOKEN HERE*"
+
+}
+
+*/
+
+
+
+
 // Require the necessary discord.js classes
 const { Client, Intents, PermissionOverwrites } = require('discord.js');
 const { token } = require('./config.json');
 //arrays surnames and lastnames are pulled from the object "data"
 const surnames = data.surname
 const lastnames = data.lastname
-
-
-
-
-/*
-    //a string from a random position is pulled from both the surnames and lastnames array
-    const sname = surnames[Math.floor(Math.random()*surnames.length)]
-    const lname = lastnames[Math.floor(Math.random()*lastnames.length)]
-*/
-
-
 
 
 
@@ -39,25 +43,27 @@ client.once("guildCreate", (g) => {
 
     let cbChannelid
 
+    // a new channel is created 
     g.channels.create("daily-benedict-cumberbatch", {
         type: 'GUILD_TEXT',
         topic: "Daily dose of Benedict Cumberbatch",
     }).then(result => {
 
+        //the id of the newly created channel is saved
         cbChannelid = result.id;
 
     })
 
-    client.channels.cache.get(cbChannelid).send(newCumberBatch())
 
+    // send a new message ever 24 hours
     setInterval(function() {
 
 
         try {
-        client.channels.cache.get(cbChannelid).send(newCumberBatch())
-        } catch (e) { console.error("Couldnt send"); } 
+            client.channels.cache.get(cbChannelid).send(newCumberBatch())
+        } catch (e) { console.log("couldnt send"); } 
 
-    },5000)
+    },86400000)
 
 
 });
